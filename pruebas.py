3b694d1c43212a52d87a068d8ff89f0fa3d9eedd
Rodrimarlon <marlonrodriguezmartinez@gmail.com>
2022-05-08 10:37:55 -0400

    Agregando Ejeccios resagados

--- pruebas.py
+++
@@ -1,9 +0,0 @@
-def convert_num_decimal(num_binary):
-    num_decimal = int(num_binary, base=2)
-    return num_decimal
-
-
-num_binary = input('Número binario: ')
-num_decimal = convert_num_decimal(num_binary)
-
-print('Número decimal: ' + str(num_decimal))
