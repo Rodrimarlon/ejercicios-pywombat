4d92d7d31be5797b0985212d45ec1d66e62e5e1f
rodrimarlon <marlonrodriguezmartinez@gmail.com>
2024-06-17 21:18:54 -0400

    volviendo despued de un tiempo

---
+++ 10_tipos_de_objetos.py
@@ -0,0 +1,4 @@
+lista = ['Hola Mundo', 234, [4, 3, 2, 1], ('H', 'o', 'l', 'a'), 25.02, {1: 'M', 2: 'u', 3: 'n', 4: 'd', 5: 'o'}]
+
+for i in lista:
+    print(type(i))
---
+++ 9_desempaquetado_de_tuplas.py
@@ -0,0 +1,5 @@
+tupla = ('Loki', 'Duke', 'Princesa', 'Lisa', 'Burns', 'Latin')
+
+primer_elemento, segundo_elemento, *args, ultimo_elemento = tupla
+
+print(primer_elemento, segundo_elemento, ultimo_elemento)
